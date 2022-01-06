abae1160674719a16a4d4bb13f0884a80e7747b7
Michael Ripley <zkxs00@gmail.com>
2022-01-06 00:02:24 -0600

    Add some docs

--- NeosModLoader/ModConfigurationKey.cs
+++ NeosModLoader/ModConfigurationKey.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NeosModLoader
 {
@@ -11,11 +12,41 @@ namespace NeosModLoader
             InternalAccessOnly = internalAccessOnly;
         }
 
+        /// <summary>
+        /// Unique name of this config item
+        /// </summary>
         public string Name { get; private set; }
+
+        /// <summary>
+        /// Human-readable description of this config item
+        /// </summary>
         public string Description { get; private set; }
+
+        /// <summary>
+        /// If true, only the owning mod should have access to this config item
+        /// </summary>
         public bool InternalAccessOnly { get; private set; }
+
+        /// <returns>Type of the config item</returns>
         public abstract Type ValueType();
+
+        /// <summary>
+        /// Checks if a value is valid for this configuration item
+        /// </summary>
+        /// <param name="value">The value to check</param>
+        /// <returns>true if the value is valid</returns>
         public abstract bool Validate(object value);
+
+        public override bool Equals(object obj)
+        {
+            return obj is ModConfigurationKey key &&
+                   Name == key.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return 539060726 + EqualityComparer<string>.Default.GetHashCode(Name);
+        }
     }
 
     public class ModConfigurationKey<T> : ModConfigurationKey
