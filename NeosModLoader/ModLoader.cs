4be5b5b8b8ecfd95eaef3350ccb9b4f51fa281f8
Michael Ripley <zkxs00@gmail.com>
2022-01-08 00:28:38 -0600

    Bump patch version

--- NeosModLoader/ModLoader.cs
+++ NeosModLoader/ModLoader.cs
@@ -12,7 +12,7 @@ namespace NeosModLoader
         /// <summary>
         /// NeosModLoader's version
         /// </summary>
-        public static readonly string VERSION = "1.6.0";
+        public static readonly string VERSION = "1.6.1";
         private static readonly Type NEOS_MOD_TYPE = typeof(NeosMod);
         internal static List<LoadedNeosMod> LoadedMods = new List<LoadedNeosMod>();
         internal static Dictionary<Assembly, NeosMod> AssemblyLookupMap = new Dictionary<Assembly, NeosMod>();
