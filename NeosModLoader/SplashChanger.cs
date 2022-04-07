aa8b0655be6d97617eae4c6b39672dfe2db30b3f
Michael Ripley <zkxs00@gmail.com>
2022-04-06 23:58:12 -0500

    run code cleanup

--- NeosModLoader/SplashChanger.cs
+++ NeosModLoader/SplashChanger.cs
@@ -10,22 +10,27 @@ namespace NeosModLoader
     {
         private static bool failed = false;
         // Returned true means success, false means something went wrong.
-        internal static bool SetCustom(string text) {
+        internal static bool SetCustom(string text)
+        {
             if (ModLoaderConfiguration.Get().HideVisuals) return true;
-            try {
+            try
+            {
                 // VerboseInit does extra logging, so turning it if off while we change the phase.
                 bool ogVerboseInit = Engine.Current.VerboseInit;
                 Engine.Current.VerboseInit = false;
                 Traverse.Create(Engine.Current)
-                    .Method("UpdateInitPhase", new Type[] {typeof(string), typeof(bool)})
+                    .Method("UpdateInitPhase", new Type[] { typeof(string), typeof(bool) })
                     .GetValue("~ NeosModLoader ~", false);
                 Traverse.Create(Engine.Current)
-                    .Method("UpdateInitSubphase", new Type[] {typeof(string), typeof(bool)})
+                    .Method("UpdateInitSubphase", new Type[] { typeof(string), typeof(bool) })
                     .GetValue(text, false);
                 Engine.Current.VerboseInit = ogVerboseInit;
                 return true;
-            } catch (Exception ex) {
-                if (!failed) {
+            }
+            catch (Exception ex)
+            {
+                if (!failed)
+                {
                     Logger.WarnInternal("Splash change failed: " + ex.ToString());
                     failed = true;
                 }
