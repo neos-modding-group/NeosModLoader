f26c5e66493034ea4ac6d10fa5e08e021db6fcc0
ljoonal <ljoonal@ljoonal.xyz>
2022-04-08 16:58:33 +0300

    Make SplashChanger not depend on Harmony

--- NeosModLoader/SplashChanger.cs
+++ NeosModLoader/SplashChanger.cs
@@ -1,6 +1,6 @@
 using FrooxEngine;
-using HarmonyLib;
 using System;
+using System.Reflection;
 
 namespace NeosModLoader
 {
@@ -9,6 +9,10 @@ namespace NeosModLoader
     internal static class SplashChanger
     {
         private static bool failed = false;
+        private static readonly MethodInfo UpdatePhase = typeof(Engine)
+            .GetMethod("UpdateInitPhase", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static readonly MethodInfo UpdateSubPhase = typeof(Engine)
+            .GetMethod("UpdateInitSubphase", BindingFlags.NonPublic | BindingFlags.Instance);
         // Returned true means success, false means something went wrong.
         internal static bool SetCustom(string text)
         {
@@ -18,12 +22,8 @@ namespace NeosModLoader
                 // VerboseInit does extra logging, so turning it if off while we change the phase.
                 bool ogVerboseInit = Engine.Current.VerboseInit;
                 Engine.Current.VerboseInit = false;
-                Traverse.Create(Engine.Current)
-                    .Method("UpdateInitPhase", new Type[] { typeof(string), typeof(bool) })
-                    .GetValue("~ NeosModLoader ~", false);
-                Traverse.Create(Engine.Current)
-                    .Method("UpdateInitSubphase", new Type[] { typeof(string), typeof(bool) })
-                    .GetValue(text, false);
+                UpdatePhase.Invoke(Engine.Current, new object[] { "~ NeosModLoader ~", false });
+                UpdateSubPhase.Invoke(Engine.Current, new object[] { text, false });
                 Engine.Current.VerboseInit = ogVerboseInit;
                 return true;
             }
