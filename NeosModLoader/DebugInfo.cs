e0357abf893731b04420e923f3b5746a17d42c91
Michael Ripley <zkxs00@gmail.com>
2022-05-05 00:16:13 -0500

    Add even more debug info

--- NeosModLoader/DebugInfo.cs
+++ NeosModLoader/DebugInfo.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection;
+using System.Runtime.Versioning;
 
 namespace NeosModLoader
 {
@@ -7,6 +9,9 @@ namespace NeosModLoader
         internal static void Log()
         {
             Logger.MsgInternal($"NeosModLoader v{ModLoader.VERSION} starting up!{(ModLoaderConfiguration.Get().Debug ? " Debug logs will be shown." : "")}");
+            Logger.MsgInternal($"CLR v{Environment.Version}");
+            Logger.DebugInternal($"Using .NET Framework: \"{AppDomain.CurrentDomain.SetupInformation.TargetFrameworkName}\"");
+            Logger.DebugInternal($"Using .NET Core: \"{Assembly.GetEntryAssembly()?.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName}\"");
             Logger.MsgInternal($"Using Harmony v{GetAssemblyVersion(typeof(HarmonyLib.Harmony))}");
             Logger.MsgInternal($"Using BaseX v{GetAssemblyVersion(typeof(BaseX.floatQ))}");
             Logger.MsgInternal($"Using FrooxEngine v{GetAssemblyVersion(typeof(FrooxEngine.IComponent))}");
