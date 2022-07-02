464cc16ed8d143673c31f6d1dfebaac684cd5db2
Michael Ripley <zkxs00@gmail.com>
2022-07-02 07:29:23 -0500

    Bump the version to 1.11.1

--- NeosModLoader/ModLoader.cs
+++ NeosModLoader/ModLoader.cs
@@ -10,7 +10,7 @@ namespace NeosModLoader
 {
     public class ModLoader
     {
-        internal const string VERSION_CONSTANT = "1.11.0";
+        internal const string VERSION_CONSTANT = "1.11.1";
         /// <summary>
         /// NeosModLoader's version
         /// </summary>
