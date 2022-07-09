9fa51efcf832072f141c514814f7950346ab4069
Michael Ripley <zkxs00@gmail.com>
2022-07-09 10:59:00 -0500

    Bump patch version

--- NeosModLoader/ModLoader.cs
+++ NeosModLoader/ModLoader.cs
@@ -10,7 +10,7 @@ namespace NeosModLoader
 {
     public class ModLoader
     {
-        internal const string VERSION_CONSTANT = "1.11.2";
+        internal const string VERSION_CONSTANT = "1.11.3";
         /// <summary>
         /// NeosModLoader's version
         /// </summary>
