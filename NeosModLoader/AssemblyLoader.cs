e7f722f0a34769ea8c238325e98a9fd22ccf8e8c
EIA485 <17285570+EIA485@users.noreply.github.com>
2022-08-04 08:55:28 -0700

    sha

--- NeosModLoader/AssemblyLoader.cs
+++ NeosModLoader/AssemblyLoader.cs
@@ -48,7 +48,7 @@ namespace NeosModLoader
             Assembly assembly;
             try
             {
-                Logger.MsgInternal( $"load assembly {filename} with 256hash: {Util.GenerateSHA256(filepath)}");
+                Logger.MsgInternal( $"load assembly {filename} with sha256hash: {Util.GenerateSHA256(filepath)}");
                 assembly = Assembly.LoadFile(filepath);
             }
             catch (Exception e)
