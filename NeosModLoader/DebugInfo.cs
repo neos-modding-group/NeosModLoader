7555f8b407aec05d9b69b754f373fe9e2345dcd1
ljoonal <ljoonal@ljoonal.xyz>
2022-08-17 18:13:44 +0300

    Propper unified formatting

--- NeosModLoader/DebugInfo.cs
+++ NeosModLoader/DebugInfo.cs
@@ -1,26 +1,26 @@
-﻿using System;
+using System;
 using System.Reflection;
 using System.Runtime.Versioning;
 
 namespace NeosModLoader
 {
-    internal class DebugInfo
+  internal class DebugInfo
+  {
+    internal static void Log()
     {
-        internal static void Log()
-        {
-            Logger.MsgInternal($"NeosModLoader v{ModLoader.VERSION} starting up!{(ModLoaderConfiguration.Get().Debug ? " Debug logs will be shown." : "")}");
-            Logger.MsgInternal($"CLR v{Environment.Version}");
-            Logger.DebugFuncInternal(() => $"Using .NET Framework: \"{AppDomain.CurrentDomain.SetupInformation.TargetFrameworkName}\"");
-            Logger.DebugFuncInternal(() => $"Using .NET Core: \"{Assembly.GetEntryAssembly()?.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName}\"");
-            Logger.MsgInternal($"Using Harmony v{GetAssemblyVersion(typeof(HarmonyLib.Harmony))}");
-            Logger.MsgInternal($"Using BaseX v{GetAssemblyVersion(typeof(BaseX.floatQ))}");
-            Logger.MsgInternal($"Using FrooxEngine v{GetAssemblyVersion(typeof(FrooxEngine.IComponent))}");
-            Logger.MsgInternal($"Using Json.NET v{GetAssemblyVersion(typeof(Newtonsoft.Json.JsonSerializer))}");
-        }
-
-        private static string? GetAssemblyVersion(Type typeFromAssembly)
-        {
-            return typeFromAssembly.Assembly.GetName()?.Version?.ToString();
-        }
+      Logger.MsgInternal($"NeosModLoader v{ModLoader.VERSION} starting up!{(ModLoaderConfiguration.Get().Debug ? " Debug logs will be shown." : "")}");
+      Logger.MsgInternal($"CLR v{Environment.Version}");
+      Logger.DebugFuncInternal(() => $"Using .NET Framework: \"{AppDomain.CurrentDomain.SetupInformation.TargetFrameworkName}\"");
+      Logger.DebugFuncInternal(() => $"Using .NET Core: \"{Assembly.GetEntryAssembly()?.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName}\"");
+      Logger.MsgInternal($"Using Harmony v{GetAssemblyVersion(typeof(HarmonyLib.Harmony))}");
+      Logger.MsgInternal($"Using BaseX v{GetAssemblyVersion(typeof(BaseX.floatQ))}");
+      Logger.MsgInternal($"Using FrooxEngine v{GetAssemblyVersion(typeof(FrooxEngine.IComponent))}");
+      Logger.MsgInternal($"Using Json.NET v{GetAssemblyVersion(typeof(Newtonsoft.Json.JsonSerializer))}");
     }
+
+    private static string? GetAssemblyVersion(Type typeFromAssembly)
+    {
+      return typeFromAssembly.Assembly.GetName()?.Version?.ToString();
+    }
+  }
 }
--- NeosModLoader/ExecutionHook.cs
+++ NeosModLoader/ExecutionHook.cs
@@ -4,54 +4,54 @@ using System.Linq;
 
 namespace NeosModLoader
 {
-    [ImplementableClass(true)]
-    internal class ExecutionHook
-    {
+  [ImplementableClass(true)]
+  internal class ExecutionHook
+  {
 #pragma warning disable CS0169
-        // fields must exist due to reflective access
-        private static Type? __connectorType; // needed in all Neos versions
-        private static Type? __connectorTypes; // needed in Neos 2021.10.17.1326 and later
+    // fields must exist due to reflective access
+    private static Type? __connectorType; // needed in all Neos versions
+    private static Type? __connectorTypes; // needed in Neos 2021.10.17.1326 and later
 #pragma warning restore CS0169
 
-        static ExecutionHook()
+    static ExecutionHook()
+    {
+      try
+      {
+        SplashChanger.SetCustom("Loading libraries");
+        AssemblyFile[] loadedAssemblies = AssemblyLoader.LoadAssembliesFromDir("nml_libs");
+        if (loadedAssemblies.Length != 0)
         {
-            try
-            {
-                SplashChanger.SetCustom("Loading libraries");
-                AssemblyFile[] loadedAssemblies = AssemblyLoader.LoadAssembliesFromDir("nml_libs");
-                if (loadedAssemblies.Length != 0)
-                {
-                    string loadedAssemblyList = string.Join("\n", loadedAssemblies.Select(a => a.Assembly.FullName));
-                    Logger.MsgInternal($"Loaded libraries from nml_libs:\n{loadedAssemblyList}");
-                }
-
-                SplashChanger.SetCustom("Initializing");
-                DebugInfo.Log();
-                NeosVersionReset.Initialize();
-                ModLoader.LoadMods();
-                SplashChanger.SetCustom("Loaded");
-            }
-            catch (Exception e) // it's important that this doesn't send exceptions back to Neos
-            {
-                Logger.ErrorInternal($"Exception in execution hook!\n{e}");
-            }
+          string loadedAssemblyList = string.Join("\n", loadedAssemblies.Select(a => a.Assembly.FullName));
+          Logger.MsgInternal($"Loaded libraries from nml_libs:\n{loadedAssemblyList}");
         }
 
-        // implementation not strictly required, but method must exist due to reflective access
-        private static DummyConnector InstantiateConnector()
-        {
-            return new DummyConnector();
-        }
-
-        // type must match return type of InstantiateConnector()
-        private class DummyConnector : IConnector
-        {
-            public IImplementable? Owner { get; private set; }
-            public void ApplyChanges() { }
-            public void AssignOwner(IImplementable owner) => Owner = owner;
-            public void Destroy(bool destroyingWorld) { }
-            public void Initialize() { }
-            public void RemoveOwner() => Owner = null;
-        }
+        SplashChanger.SetCustom("Initializing");
+        DebugInfo.Log();
+        NeosVersionReset.Initialize();
+        ModLoader.LoadMods();
+        SplashChanger.SetCustom("Loaded");
+      }
+      catch (Exception e) // it's important that this doesn't send exceptions back to Neos
+      {
+        Logger.ErrorInternal($"Exception in execution hook!\n{e}");
+      }
     }
+
+    // implementation not strictly required, but method must exist due to reflective access
+    private static DummyConnector InstantiateConnector()
+    {
+      return new DummyConnector();
+    }
+
+    // type must match return type of InstantiateConnector()
+    private class DummyConnector : IConnector
+    {
+      public IImplementable? Owner { get; private set; }
+      public void ApplyChanges() { }
+      public void AssignOwner(IImplementable owner) => Owner = owner;
+      public void Destroy(bool destroyingWorld) { }
+      public void Initialize() { }
+      public void RemoveOwner() => Owner = null;
+    }
+  }
 }
