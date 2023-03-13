a43c25a54486b4378a47ba556fc7abcf85fa1290
Arne Kiesewetter <banane9@live.de>
2023-03-13 02:02:00 +0100

    It's documentin time (for generic method invokers)

--- NeosModLoader/Utility/GenericMethodInvoker.cs
+++ NeosModLoader/Utility/GenericMethodInvoker.cs
@@ -6,16 +6,28 @@ using System.Threading.Tasks;
 
 namespace NeosModLoader.Utility
 {
-	public sealed class GenericMethodInvoker<TInstance, TReturn>
+	/// <summary>
+	/// Represents the base class for more specific generic method invokers.
+	/// </summary>
+	/// <typeparam name="TInstance">The type of the instances that the generic method gets invoked on.</typeparam>
+	/// <typeparam name="TReturn">The type of the generic method's return value. Use <c>object</c> if it depends on the generic type parameters of the method.</typeparam>
+	public abstract class BaseGenericMethodInvoker<TInstance, TReturn>
 	{
 		private readonly Dictionary<TypeDefinition, MethodInfo> concreteMethods = new();
 
+		/// <summary>
+		/// Gets the generic <see cref="MethodInfo"/> who's concrete versions will be in invoked by this generic method invoker.
+		/// </summary>
 		public MethodInfo GenericMethod { get; }
 
-		public GenericMethodInvoker(MethodInfo method) : this(method, false)
-		{ }
-
-		internal GenericMethodInvoker(MethodInfo method, bool ignoreLackOfGenericParameters)
+		/// <summary>
+		/// Creates a new generic method invoker for the given method which may not have generic type parameters.
+		/// <para/>
+		/// Use <c>object</c> for <typeparamref name="TReturn"/> if it depends on the generic type parameters of the method.
+		/// </summary>
+		/// <param name="method">The generic method to invoke concrete version of.</param>
+		/// <param name="ignoreLackOfGenericParameters">Ignores the method lacking generic type parameters if <c>true</c>.</param>
+		internal BaseGenericMethodInvoker(MethodInfo method, bool ignoreLackOfGenericParameters)
 		{
 			if (!ignoreLackOfGenericParameters && !method.ContainsGenericParameters)
 				throw new ArgumentException("Target method must have remaining open type parameters.", nameof(method));
@@ -23,12 +35,25 @@ namespace NeosModLoader.Utility
 			GenericMethod = method;
 		}
 
-		public TReturn Invoke(TInstance instance, TypeDefinition definition, params object[] parameters)
-		{
-			return InvokeInternal(instance, definition, parameters);
-		}
+		/// <summary>
+		/// Creates a new generic method invoker for the given open generic method.
+		/// <para/>
+		/// Use <c>object</c> for <typeparamref name="TReturn"/> if it depends on the generic type parameters of the method.
+		/// </summary>
+		/// <param name="method">The generic method to invoke concrete version of.</param>
+		internal BaseGenericMethodInvoker(MethodInfo method) : this(method, false)
+		{ }
 
-		internal TReturn InvokeInternal(TInstance? instance, TypeDefinition definition, object[]? parameters)
+		/// <summary>
+		/// Invokes a concrete version of this invoker's <see cref="GenericMethod">GenericMethod</see> using the given <paramref name="instance"/>,
+		/// type parameter <paramref name="definition"/> and method <paramref name="parameters"/>.
+		/// </summary>
+		/// <param name="instance">The object instance to invoke the method on. Use <c>null</c> for static methods.</param>
+		/// <param name="definition">The generic type parameter definition to create the concrete method with.<br/>
+		/// May be ignored if <see cref="GenericMethod"/> doesn't contain generic parameters.</param>
+		/// <param name="parameters">The parameters to invoke the method with. May be empty or null if there is none.</param>
+		/// <returns>The result of the method invocation.</returns>
+		internal TReturn InvokeInternal(TInstance? instance, TypeDefinition definition, params object[]? parameters)
 		{
 			if (!concreteMethods.TryGetValue(definition, out var method))
 			{
@@ -44,55 +69,119 @@ namespace NeosModLoader.Utility
 		}
 	}
 
-	public sealed class GenericMethodInvoker<TReturn>
+	/// <summary>
+	/// Represents a generic method invoker that invokes instance methods with a return value.
+	/// </summary>
+	/// <inheritdoc/>
+	public sealed class GenericInstanceMethodInvoker<TInstance, TReturn> : BaseGenericMethodInvoker<TInstance, TReturn>
 	{
-		private readonly Dictionary<TypeDefinition, MethodInfo> concreteMethods = new();
+		/// <summary>
+		/// Creates a new generic method invoker for the given open generic instance method with a return value.
+		/// <para/>
+		/// Use <c>object</c> for <typeparamref name="TReturn"/> if it depends on the generic type parameters of the method.
+		/// </summary>
+		/// <param name="method">The generic method to invoke concrete version of.</param>
+		public GenericInstanceMethodInvoker(MethodInfo method) : base(method)
+		{ }
 
-		public MethodInfo GenericMethod { get; }
+		internal GenericInstanceMethodInvoker(MethodInfo method, bool ignoreLackOfGenericParameters)
+					: base(method, ignoreLackOfGenericParameters)
+		{ }
 
-		public GenericMethodInvoker(MethodInfo method)
+		/// <summary>
+		/// Invokes a concrete version of this invoker's
+		/// <see cref="BaseGenericMethodInvoker{TInstance, TReturn}.GenericMethod">GenericMethod</see>
+		/// using the given <paramref name="instance"/>, type parameter <paramref name="definition"/> and method <paramref name="parameters"/>.
+		/// </summary>
+		/// <param name="instance">The object instance to invoke the method on.</param>
+		/// <param name="definition">The generic type parameter definition to create the concrete method with.</param>
+		/// <param name="parameters">The parameters to invoke the method with.</param>
+		/// <returns>The result of the method invocation.</returns>
+		public TReturn Invoke(TInstance instance, TypeDefinition definition, params object[] parameters)
 		{
-			if (!method.ContainsGenericParameters)
-				throw new ArgumentException("Target method must have remaining open type parameters.", nameof(method));
-
-			GenericMethod = method;
-		}
-
-		public TReturn Invoke(TypeDefinition definition, params object[] parameters)
-		{
-			if (!concreteMethods.TryGetValue(definition, out var method))
-			{
-				method = GenericMethod.MakeGenericMethod(definition.types);
-				concreteMethods.Add(definition, method);
-			}
-
-			return (TReturn)method.Invoke(null, parameters);
+			return InvokeInternal(instance, definition, parameters);
 		}
 	}
 
-	public sealed class GenericMethodInvoker
+	/// <summary>
+	/// Represents a generic method invoker that invokes static methods with a return value.
+	/// </summary>
+	/// <inheritdoc/>
+	public sealed class GenericStaticMethodInvoker<TReturn> : BaseGenericMethodInvoker<object, TReturn>
 	{
-		private readonly Dictionary<TypeDefinition, MethodInfo> concreteMethods = new();
+		/// <summary>
+		/// Creates a new generic method invoker for the given open generic static method with a return value.
+		/// <para/>
+		/// Use <c>object</c> for <typeparamref name="TReturn"/> if it depends on the generic type parameters of the method.
+		/// </summary>
+		/// <param name="method">The generic method to invoke concrete version of.</param>
+		public GenericStaticMethodInvoker(MethodInfo method) : base(method)
+		{ }
 
-		public MethodInfo GenericMethod { get; }
-
-		public GenericMethodInvoker(MethodInfo method)
+		/// <summary>
+		/// Invokes a concrete version of this invoker's static
+		/// <see cref="BaseGenericMethodInvoker{TInstance, TReturn}.GenericMethod">GenericMethod</see>
+		/// using the given type parameter <paramref name="definition"/> and method <paramref name="parameters"/>.
+		/// </summary>
+		/// <param name="definition">The generic type parameter definition to create the concrete method with.</param>
+		/// <param name="parameters">The parameters to invoke the method with.</param>
+		/// <returns>The result of the method invocation.</returns>
+		public TReturn Invoke(TypeDefinition definition, params object[] parameters)
 		{
-			if (!method.ContainsGenericParameters)
-				throw new ArgumentException("Target method must have remaining open type parameters.", nameof(method));
-
-			GenericMethod = method;
+			return InvokeInternal(null, definition, parameters);
 		}
+	}
 
+	/// <summary>
+	/// Represents a generic method invoker that invokes static void methods.
+	/// </summary>
+	public sealed class GenericStaticVoidMethodInvoker : BaseGenericMethodInvoker<object, object>
+	{
+		/// <summary>
+		/// Creates a new generic method invoker for the given open generic static void method.
+		/// </summary>
+		/// <param name="method">The generic method to invoke concrete version of.</param>
+		public GenericStaticVoidMethodInvoker(MethodInfo method) : base(method)
+		{ }
+
+		/// <summary>
+		/// Invokes a concrete version of this invoker's static void
+		/// <see cref="BaseGenericMethodInvoker{TInstance, TReturn}.GenericMethod">GenericMethod</see>
+		/// using the given type parameter <paramref name="definition"/> and method <paramref name="parameters"/>.
+		/// </summary>
+		/// <param name="definition">The generic type parameter definition to create the concrete method with.</param>
+		/// <param name="parameters">The parameters to invoke the method with.</param>
+		/// <returns>The result of the method invocation.</returns>
 		public void Invoke(TypeDefinition definition, params object[] parameters)
 		{
-			if (!concreteMethods.TryGetValue(definition, out var method))
-			{
-				method = GenericMethod;
-				concreteMethods.Add(definition, method);
-			}
+			InvokeInternal(null, definition, parameters);
+		}
+	}
 
-			method.Invoke(null, parameters);
+	/// <summary>
+	/// Represents a generic method invoker that invokes instance void methods.
+	/// </summary>
+	/// <inheritdoc/>
+	public sealed class GenericVoidMethodInvoker<TInstance> : BaseGenericMethodInvoker<TInstance, object>
+	{
+		/// <summary>
+		/// Creates a new generic method invoker for the given open generic instance void method.
+		/// </summary>
+		/// <param name="method">The generic method to invoke concrete version of.</param>
+		public GenericVoidMethodInvoker(MethodInfo method) : base(method)
+		{ }
+
+		/// <summary>
+		/// Invokes a concrete version of this invoker's void
+		/// <see cref="BaseGenericMethodInvoker{TInstance, TReturn}.GenericMethod">GenericMethod</see>
+		/// using the given <paramref name="instance"/>, type parameter <paramref name="definition"/> and method <paramref name="parameters"/>.
+		/// </summary>
+		/// <param name="instance">The object instance to invoke the method on.</param>
+		/// <param name="definition">The generic type parameter definition to create the concrete method with.</param>
+		/// <param name="parameters">The parameters to invoke the method with.</param>
+		public void Invoke(TInstance instance, TypeDefinition definition, params object[] parameters)
+		{
+			InvokeInternal(instance, definition, parameters);
 		}
 	}
 }
--- NeosModLoader/Utility/GenericTypeMethodsInvoker.cs
+++ NeosModLoader/Utility/GenericTypeMethodsInvoker.cs
@@ -9,52 +9,193 @@ using System.Threading.Tasks;
 
 namespace NeosModLoader.Utility
 {
+	/// <summary>
+	/// Represents a generic method invoker that invokes potentially generic methods on generic types.
+	/// </summary>
 	public sealed class GenericTypeMethodsInvoker
 	{
 		private readonly Dictionary<TypeDefinition, ConcreteType> concreteTypes = new();
 
+		/// <summary>
+		/// Gets the generic <see cref="Type"/> who's concrete versions will have concrete methods invoked by this invoker.
+		/// </summary>
 		public Type GenericType { get; }
 
+		/// <summary>
+		/// Gets the function that handles resolving passed in <see cref="MethodInfo"/>s of the <see cref="GenericType">GenericType</see> to ones of the concrete one.
+		/// </summary>
 		public Func<MethodInfo, Type, MethodInfo> GetGenericMethodOfConcreteType { get; }
 
+		/// <summary>
+		/// Creates a new generic type methods invoker with the given generic type and the default concrete method resolving function.
+		/// </summary>
+		/// <param name="genericType">The generic type who's concrete versions will have concrete methods invoked by this invoker.</param>
 		public GenericTypeMethodsInvoker(Type genericType)
 			: this(genericType, GetGenericMethodOfConcreteTypeDefault)
 		{ }
 
+		/// <summary>
+		/// Creates a new generic type methods invoker with the given generic type and method resolving function.
+		/// </summary>
+		/// <param name="genericType">The generic type who's concrete versions will have concrete methods invoked by this invoker.</param>
+		/// <param name="getGenericMethodOfConcreteType">The function that handles resolving passed in <see cref="MethodInfo"/>s of the <see cref="GenericType">GenericType</see> to ones of the concrete one.</param>
 		public GenericTypeMethodsInvoker(Type genericType, Func<MethodInfo, Type, MethodInfo> getGenericMethodOfConcreteType)
 		{
 			GenericType = genericType;
 			GetGenericMethodOfConcreteType = getGenericMethodOfConcreteType;
 		}
 
+		/// <summary>
+		/// Invokes a concrete version of the given generic method using the given <paramref name="instance"/>,
+		/// <paramref name="methodDefinition"/> type parameters and method <paramref name="parameters"/> on the
+		/// concrete version of this instance's <see cref="GenericType">GenericType</see> created with the <paramref name="instanceDefinition"/> type parameters.
+		/// </summary>
+		/// <typeparam name="TReturn">The invoked method's return type.</typeparam>
+		/// <param name="method">The generic method of the generic type to invoke.</param>
+		/// <param name="instanceDefinition">The generic type parameter definition to create the concrete instance type with.</param>
+		/// <param name="instance">The instance to invoke the constructed concrete method on.</param>
+		/// <param name="methodDefinition">The generic type parameter definition to create the concrete method with.</param>
+		/// <param name="parameters">The parameters to invoke the method with. May be empty if there is none.</param>
+		/// <returns>The invoked method's return value.</returns>
 		public TReturn Invoke<TReturn>(MethodInfo method, TypeDefinition instanceDefinition, object instance, TypeDefinition methodDefinition, params object[] parameters)
 		{
 			return (TReturn)InvokeInternal(method, instanceDefinition, instance, methodDefinition, parameters);
 		}
 
+		/// <summary>
+		/// Invokes a concrete version of the given generic method using the given <paramref name="instance"/>,
+		/// <paramref name="methodDefinition"/> type parameters and method <paramref name="parameters"/> on the
+		/// concrete version of this instance's <see cref="GenericType">GenericType</see> created with the <paramref name="instanceDefinition"/> type parameters.
+		/// </summary>
+		/// <param name="method">The generic method of the generic type to invoke.</param>
+		/// <param name="instanceDefinition">The generic type parameter definition to create the concrete instance type with.</param>
+		/// <param name="instance">The instance to invoke the constructed concrete method on.</param>
+		/// <param name="methodDefinition">The generic type parameter definition to create the concrete method with.</param>
+		/// <param name="parameters">The parameters to invoke the method with. May be empty if there is none.</param>
+		/// <returns>The invoked method's return value.</returns>
 		public object Invoke(MethodInfo method, TypeDefinition instanceDefinition, object instance, TypeDefinition methodDefinition, params object[] parameters)
 		{
 			return InvokeInternal(method, instanceDefinition, instance, methodDefinition, parameters);
 		}
 
+		/// <summary>
+		/// Invokes a concrete version of the given method on the concrete version of this instance's
+		/// <see cref="GenericType">GenericType</see> created with the <paramref name="instanceDefinition"/> type parameters.
+		/// </summary>
+		/// <typeparam name="TReturn">The invoked method's return type.</typeparam>
+		/// <param name="method">The generic method of the generic type to invoke.</param>
+		/// <param name="instanceDefinition">The generic type parameter definition to create the concrete instance type with.</param>
+		/// <param name="instance">The instance to invoke the constructed concrete method on.</param>
+		/// <param name="parameters">The parameters to invoke the method with. May be empty if there is none.</param>
+		/// <returns>The invoked method's return value.</returns>
 		public TReturn Invoke<TReturn>(MethodInfo method, TypeDefinition instanceDefinition, object instance, params object[] parameters)
 		{
 			return (TReturn)InvokeInternal(method, instanceDefinition, instance, new TypeDefinition(), parameters);
 		}
 
+		/// <summary>
+		/// Invokes a concrete version of the given static method of the concrete version of this instance's
+		/// <see cref="GenericType">GenericType</see> created with the <paramref name="instanceDefinition"/> type parameters.
+		/// </summary>
+		/// <typeparam name="TReturn">The invoked method's return type.</typeparam>
+		/// <param name="method">The generic method of the generic type to invoke.</param>
+		/// <param name="instanceDefinition">The generic type parameter definition to create the concrete instance type with.</param>
+		/// <param name="parameters">The parameters to invoke the method with. May be empty if there is none.</param>
+		/// <returns>The invoked method's return value.</returns>
 		public TReturn Invoke<TReturn>(MethodInfo method, TypeDefinition instanceDefinition, params object[] parameters)
 		{
 			return (TReturn)InvokeInternal(method, instanceDefinition, null, new TypeDefinition(), parameters);
 		}
 
+		/// <summary>
+		/// Invokes a concrete version of the given method on the concrete version of this instance's
+		/// <see cref="GenericType">GenericType</see> created with the <paramref name="instanceDefinition"/> type parameters.
+		/// </summary>
+		/// <param name="method">The generic method of the generic type to invoke.</param>
+		/// <param name="instanceDefinition">The generic type parameter definition to create the concrete instance type with.</param>
+		/// <param name="instance">The instance to invoke the constructed concrete method on.</param>
+		/// <param name="parameters">The parameters to invoke the method with. May be empty if there is none.</param>
+		/// <returns>The invoked method's return value.</returns>
 		public object Invoke(MethodInfo method, TypeDefinition instanceDefinition, object instance, params object[] parameters)
 		{
 			return InvokeInternal(method, instanceDefinition, instance, new TypeDefinition(), parameters);
 		}
 
+		/// <summary>
+		/// Invokes a concrete version of the given static method of the concrete version of this instance's
+		/// <see cref="GenericType">GenericType</see> created with the <paramref name="instanceDefinition"/> type parameters.
+		/// </summary>
+		/// <typeparam name="TReturn">The invoked method's return type.</typeparam>
+		/// <param name="method">The generic method of the generic type to invoke.</param>
+		/// <param name="instanceDefinition">The generic type parameter definition to create the concrete instance type with.</param>
+		/// <param name="parameters">The parameters to invoke the method with. May be empty if there is none.</param>
+		/// <returns>The invoked method's return value.</returns>
 		public object Invoke(MethodInfo method, TypeDefinition instanceDefinition, params object[] parameters)
 		{
 			return InvokeInternal(method, instanceDefinition, null, new TypeDefinition(), parameters);
+		}
+
+		/// <summary>
+		/// Invokes a concrete version of the given method on the concrete version of this instance's
+		/// <see cref="GenericType">GenericType</see> created with the given <paramref name="instance"/>'s type parameters.
+		/// </summary>
+		/// <param name="method">The generic method of the generic type to invoke.</param>
+		/// <param name="instance">The instance to invoke the constructed concrete method on.</param>
+		/// <param name="parameters">The parameters to invoke the method with. May be empty if there is none.</param>
+		/// <returns>The invoked method's return value.</returns>
+		/// <exception cref="ArgumentException">The <paramref name="instance"/>is not descended from the <see cref="GenericType">GenericType</see>.</exception>
+		public object Invoke(MethodInfo method, object instance, params object[] parameters)
+		{
+			return InvokeInternal(method, GetMatchingGenericTypeArguments(instance), instance, new TypeDefinition(), parameters);
+		}
+
+		/// <summary>
+		/// Invokes a concrete version of the given generic method using the given <paramref name="instance"/>,
+		/// <paramref name="methodDefinition"/> type parameters and method <paramref name="parameters"/> on the
+		/// concrete version of this instance's <see cref="GenericType">GenericType</see> created with the given <paramref name="instance"/>'s type parameters.
+		/// </summary>
+		/// <typeparam name="TReturn">The invoked method's return type.</typeparam>
+		/// <param name="method">The generic method of the generic type to invoke.</param>
+		/// <param name="instance">The instance to invoke the constructed concrete method on.</param>
+		/// <param name="methodDefinition">The generic type parameter definition to create the concrete method with.</param>
+		/// <param name="parameters">The parameters to invoke the method with. May be empty if there is none.</param>
+		/// <returns>The invoked method's return value.</returns>
+		/// <exception cref="ArgumentException">The <paramref name="instance"/>is not descended from the <see cref="GenericType">GenericType</see>.</exception>
+		public object Invoke(MethodInfo method, object instance, TypeDefinition methodDefinition, params object[] parameters)
+		{
+			return InvokeInternal(method, GetMatchingGenericTypeArguments(instance), instance, methodDefinition, parameters);
+		}
+
+		/// <summary>
+		/// Invokes a concrete version of the given generic method using the given <paramref name="instance"/>,
+		/// <paramref name="methodDefinition"/> type parameters and method <paramref name="parameters"/> on the
+		/// concrete version of this instance's <see cref="GenericType">GenericType</see> created with the given <paramref name="instance"/>'s type parameters.
+		/// </summary>
+		/// <typeparam name="TReturn">The invoked method's return type.</typeparam>
+		/// <param name="method">The generic method of the generic type to invoke.</param>
+		/// <param name="instance">The instance to invoke the constructed concrete method on.</param>
+		/// <param name="methodDefinition">The generic type parameter definition to create the concrete method with.</param>
+		/// <param name="parameters">The parameters to invoke the method with. May be empty if there is none.</param>
+		/// <returns>The invoked method's return value.</returns>
+		/// <exception cref="ArgumentException">The <paramref name="instance"/>is not descended from the <see cref="GenericType">GenericType</see>.</exception>
+		public TReturn Invoke<TReturn>(MethodInfo method, object instance, TypeDefinition methodDefinition, params object[] parameters)
+		{
+			return (TReturn)InvokeInternal(method, GetMatchingGenericTypeArguments(instance), instance, methodDefinition, parameters);
+		}
+
+		/// <summary>
+		/// Invokes a concrete version of the given method on the concrete version of this instance's
+		/// <see cref="GenericType">GenericType</see> created with the given <paramref name="instance"/>'s type parameters.
+		/// </summary>
+		/// <typeparam name="TReturn">The invoked method's return type.</typeparam>
+		/// <param name="method">The generic method of the generic type to invoke.</param>
+		/// <param name="instance">The instance to invoke the constructed concrete method on.</param>
+		/// <param name="parameters">The parameters to invoke the method with. May be empty if there is none.</param>
+		/// <returns>The invoked method's return value.</returns>
+		/// <exception cref="ArgumentException">The <paramref name="instance"/>is not descended from the <see cref="GenericType">GenericType</see>.</exception>
+		public TReturn Invoke<TReturn>(MethodInfo method, object instance, params object[] parameters)
+		{
+			return (TReturn)InvokeInternal(method, GetMatchingGenericTypeArguments(instance), instance, new TypeDefinition(), parameters);
 		}
 
 		private static MethodInfo GetGenericMethodOfConcreteTypeDefault(MethodInfo needleMethod, Type concreteType)
@@ -96,6 +237,24 @@ namespace NeosModLoader.Utility
 				});
 		}
 
+		private Type[] GetMatchingGenericTypeArguments(object instance)
+		{
+			var type = instance.GetType();
+
+			do
+			{
+				if (type.IsGenericType && type.GetGenericTypeDefinition() == GenericType)
+					return type.GenericTypeArguments;
+
+				type = type.BaseType;
+			}
+			while (type != null);
+
+			throw new ArgumentException(
+				$"Provided instance [{instance.GetType().FullDescription()}] was not descended from {nameof(GenericType)} [{GenericType.FullDescription()}].",
+				nameof(instance));
+		}
+
 		private object InvokeInternal(MethodInfo method, TypeDefinition instanceTypes, object? instance, TypeDefinition methodTypes, object[] parameters)
 		{
 			if (!concreteTypes.TryGetValue(instanceTypes, out var concreteType))
@@ -111,7 +270,7 @@ namespace NeosModLoader.Utility
 
 		private readonly struct ConcreteType
 		{
-			public readonly Dictionary<MethodInfo, GenericMethodInvoker<object, object>> MethodInvokers = new();
+			public readonly Dictionary<MethodInfo, BaseGenericMethodInvoker<object, object>> MethodInvokers = new();
 			public readonly Type Type;
 
 			public ConcreteType(Type type)
@@ -121,11 +280,11 @@ namespace NeosModLoader.Utility
 
 			public static implicit operator ConcreteType(Type type) => new(type);
 
-			public GenericMethodInvoker<object, object> GetMethodInvoker(MethodInfo genericMethod, Func<MethodInfo, Type, MethodInfo> getMethod)
+			public BaseGenericMethodInvoker<object, object> GetMethodInvoker(MethodInfo genericMethod, Func<MethodInfo, Type, MethodInfo> getMethod)
 			{
 				if (!MethodInvokers.TryGetValue(genericMethod, out var methodInvoker))
 				{
-					methodInvoker = new GenericMethodInvoker<object, object>(getMethod(genericMethod, Type), true);
+					methodInvoker = new GenericInstanceMethodInvoker<object, object>(getMethod(genericMethod, Type), true);
 					MethodInvokers.Add(genericMethod, methodInvoker);
 				}
 
